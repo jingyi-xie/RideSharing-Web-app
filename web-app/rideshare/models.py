77a2633c048d4d9b69388843f2ad0a51e426df25
fh65 <fh65@duke.edu>
2020-01-23 10:36:57 -0500

    Add model

--- web-app/rideshare/models.py
+++ web-app/rideshare/models.py
@@ -1,3 +1,35 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length = 100)
+    user_type = models.CharField()
+    user_special = models.CharField()
+    user_share = models.BooleanField()
+    
+    vehicle_plate = models.CharField()
+    vehicle_type = models.CharField()
+    vehicle_capacity = models.IntegerField()
+    vehicle_special = models.CharField()
+    
+    def  __str__(self):
+        return self.user_name
+
+class Ride(model.Model):
+    status = models.CharField()
+    dest = models.CharField()
+    arrival = models.DateTimeField()
+
+    num_passenger = models.IntegerField()
+    sharable = models.BooleanField()
+    user_special = models.ForeignKey(User)
+    vehicle_special = models.ForeignKey(User)
+
+    driver = models.ForeignKey(User)
+    owner = models.ForeignKey(User)
+    sharer = models.ForeignKey(User)
+
+    driver_id = models.uuidField()
+    owner_id = models.uuidField()
+    sharer_id = models.uuidField()
