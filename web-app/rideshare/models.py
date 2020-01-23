3abeb77d35e69cc76ee65ee23ae8542b8fd12851
Jingyi Xie <jingyi.xie@duke.com>
2020-01-23 18:45:25 -0500

    fixed template doesn't exist

--- web-app/rideshare/models.py
+++ web-app/rideshare/models.py
@@ -2,7 +2,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from django.contrib.postgres.fields import ArrayField
-
+"""
 class User(models.Model):
     user_name = models.CharField(max_length = 100)
     user_type = models.CharField()
@@ -17,7 +17,7 @@ class User(models.Model):
     def  __str__(self):
         return self.user_name
 
-class Ride(model.Model):
+class Ride(models.Model):
     status = models.CharField()
     dest = models.CharField()
     arrival = models.DateTimeField()
@@ -32,3 +32,4 @@ class Ride(model.Model):
     sharer = models.ForeignKey(User)
 
     passenger_list = ArrayField(models.CharField())
+"""
