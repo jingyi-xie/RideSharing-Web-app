941940c3f7a8d26cb38f21e74448da7b3e4dbf3f
Jingyi Xie <jingyi.xie@duke.com>
2020-01-24 16:46:19 -0500

    update special request

--- web-app/rideshare/forms.py
+++ web-app/rideshare/forms.py
@@ -3,4 +3,4 @@ from django import forms
 from .models import User
 
 class UserForm(forms.Form):
-    user_special = forms.CharField(label = 'Special Request')
+    user_special = forms.CharField(label = 'Special Request', widget=forms.Textarea())
