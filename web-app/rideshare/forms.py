98cf9c0396b7a393f9561bbbe37ba46ca4a9f1f2
Jingyi Xie <jingyi.xie@duke.com>
2020-01-26 00:50:55 -0500

    start signup

--- web-app/rideshare/forms.py
+++ web-app/rideshare/forms.py
@@ -2,5 +2,17 @@ from django import forms
 
 from .models import User
 
+
 class UserForm(forms.Form):
-    user_special = forms.CharField(label = 'Special Request', widget=forms.Textarea())
+    user_name = forms.CharField(label = 'User Name')
+    user_special = forms.CharField(label = 'Special Request', required = False, widget=forms.Textarea())
+"""
+class SignUpForm(forms.UserCreationForm):
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+    email = forms.EmailField(max_length=250)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+"""
