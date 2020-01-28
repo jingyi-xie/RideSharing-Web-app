31e6f038df838cc1d4b8de3b5310c13848f7e727
fh65 <fh65@duke.edu>
2020-01-27 19:37:40 -0500

    Modify model

--- web-app/rideshare/models.py
+++ web-app/rideshare/models.py
@@ -1,35 +1,31 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 
-class User(models.Model):
-    user_name = models.CharField(max_length = 100)
-    #user_type = models.CharField(max_length = 100)
-    user_special = models.CharField(max_length = 500)
-    user_share = models.BooleanField(default = False)
-    
+class app_user(models.Model):
+    user_name = models.OneToOneField(User, on_delete = models.CASCADE, related_name = "username")
+    will_drive = models.BooleanField(default = False)
     vehicle_plate = models.CharField(max_length = 100)
     vehicle_type = models.CharField(max_length = 100)
-    vehicle_capacity = models.IntegerField(default = 0)
+    vehicle_capacity = models.IntegerField(default = 4)
     vehicle_special = models.CharField(max_length = 500)
     
     def  __str__(self):
         return self.user_name
 
-class Ride(models.Model):
+class app_ride(models.Model):
     status = models.CharField(max_length = 100)
     dest = models.CharField(max_length = 100)
     arrival = models.DateTimeField()
 
     num_passenger = models.IntegerField(default = 0)
     sharable = models.BooleanField(default = False)
-    user_special = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "user_request")
-    vehicle_special = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "vehicle_request")
+    user_special = models.CharField(max_leng = 500, blank = True)
 
     driver = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name = "driver")
     owner = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name = "owner")
-    sharer = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name = "sharer")
 
     passenger_list = ArrayField(models.CharField(max_length = 100))
 
