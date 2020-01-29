85d48678dff78594faefbed341ea570def3fe528
Jingyi Xie <jingyi.xie@duke.com>
2020-01-29 00:23:22 -0500

    fixed createview

--- web-app/rideshare/views.py
+++ web-app/rideshare/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import UserSignupForm, UserForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import app_user, app_ride
 from django.views.generic import CreateView, ListView, DetailView
 
@@ -16,8 +17,9 @@ def signup_view(request):
         form = UserSignupForm
     return render(request, 'rideshare/signup.html', {'form': form})
 
-class ride_request_view(CreateView):
+class ride_request_view(LoginRequiredMixin, CreateView):
     model = app_ride
+    success_url = '/riderequest'
     fields = ['dest', 'arrival','sharable', 'v_type', 'user_special']
     def form_valid(self, form):
         form.instance.owner = self.request.user
