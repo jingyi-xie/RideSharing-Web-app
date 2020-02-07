ff05d135b39e5d8a02cab7ba4e27b2fe3f4efb38
Jingyi Xie <jingyi.xie@duke.com>
2020-02-07 11:49:19 -0500

    change url

--- web-app/rideshare/urls.py
+++ web-app/rideshare/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('signup/', signup_view, name = 'signup'),
     path('logout/', auth_views.LogoutView.as_view(template_name='rideshare/logout.html'), name = 'logout'),
     path('login/', auth_views.LoginView.as_view(template_name='rideshare/login.html'), name = 'login'),
-    path('profile/', profile_view, name = 'profile'),
+    path('', profile_view, name = 'profile'),
     path('profile/update/', profile_update_view, name = 'profileupdate'),
     path('ride/<int:pk>/', ride_detail_view.as_view(), name = 'ridedetail'),
     path('ride/<int:pk>/edit/', ride_edit_view.as_view(), name = 'rideedit'),
